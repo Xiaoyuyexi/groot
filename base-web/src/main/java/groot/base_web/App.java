ea2309af52c3a6797fb3be7d553f70ece58af8b9
bigflat <zhaodaping@gmail.com>
2014-10-22 21:24:17 +0800

    项目提交

---
+++ base-web/src/main/java/groot/base_web/App.java
@@ -0,0 +1,13 @@
+package groot.base_web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
