a971aade89070da08747f744dff429d4b68c6aa2
grootzhao <gudu59@163.com>
2014-10-23 23:36:18 +0800

    why can not merge

--- base-web/src/main/java/com/groot/web/Test.java
+++ base-web/src/main/java/com/groot/web/Test.java
@@ -3,6 +3,7 @@ package com.groot.web;
 public class Test {
 
 	public static void main(String[] args){
-		System.out.println("branch just pull request")
+		System.out.println("branch just pull request");
+		System.out.println("why?");
 	}
 }
